3b3cd1f7098d9b2bc4c87e064134f6ba6bbb3522
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-06-30 15:44:12 -0300

    new InitDoctor migration

---
+++ src/database/migrations/1624996870961-initDoctor.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class DoctorInit1624989539133 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "doctors",
+            columns: [{
+                name: "id",
+                type: "uuid",
+                isPrimary: true
+            }, {
+                name: "name",
+                type: "varchar"
+            }, {
+                name: "crm",
+                type: "varchar"
+            }, {
+                name: "landline_phone",
+                type: "varchar"
+            }, {
+                name: "mobile_phone",
+                type: "varchar"
+            }, {
+                name: "cep",
+                type: "varchar"
+            }, {
+
+                name: 'created_at',
+                type: 'timestamp',
+                default: 'now()'
+            }, {
+
+                name: 'updated_at',
+                type: 'timestamp',
+                default: 'now()'
+            }, {
+                name: 'deleted_at',
+                type: 'timestamp',
+                isNullable: true
+            }]
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("doctors");
+    }
+
+}
