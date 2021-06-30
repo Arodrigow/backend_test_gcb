78666ed447437ea5e21f09e10b19d0f5afa21d77
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-06-30 15:43:22 -0300

    docker config fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { DoctorModule } from './doctor/doctor.module';
 
+
 @Module({
-  imports: [DoctorModule, TypeOrmModule.forRoot()],
+  imports: [DoctorModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      username: "postgres",
+      password: "docker",
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: false,
+      synchronize: false,
+      entities: [__dirname + "/**/entities/*.entity{.ts,.js}"],
+    })],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private connection: Connection) { }
+}
