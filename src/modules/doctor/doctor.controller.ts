b41c43d103b5dd3fbf1033db7f1188ee57e82340
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-07-12 10:14:00 -0300

    [DONE] Documentation to doc e spec controllers

--- src/modules/doctor/doctor.controller.ts
+++ src/modules/doctor/doctor.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
-import { ApiResponse, ApiTags } from "@nestjs/swagger"
+import { ApiResponse, ApiTags, ApiQuery } from "@nestjs/swagger"
 import { DoctorService } from './doctor.service';
 import { SaveDoctorDto } from './dto/save-doctor.dto';
 import { UpdateDoctorDto } from './dto/update-doctor.dto';
@@ -16,25 +16,48 @@ export class DoctorController {
     return await this.doctorService.create(saveDoctorDto);
   }
 
+  @ApiResponse({ status: 200, description: 'A doctor has been found.' })
+  @ApiResponse({ status: 404, description: "Requested doctor does not exist" })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.doctorService.findOne(id);
   }
 
+
   @Get('search/q')
+  @ApiResponse({ status: 200, description: 'Search successfull.' })
+  @ApiQuery({ name: "name", required: false })
+  @ApiQuery({ name: "crm", required: false })
+  @ApiQuery({ name: "landline_phone", required: false })
+  @ApiQuery({ name: "mobile_phone", required: false })
+  @ApiQuery({ name: "cep", required: false })
+  @ApiQuery({ name: "end", required: false })
+  @ApiQuery({ name: "bairro", required: false })
+  @ApiQuery({ name: "cidade", required: false })
+  @ApiQuery({ name: "uf", required: false })
+  @ApiQuery({ name: "specialty", required: false })
   search(@Query() params) {
     return this.doctorService.search(params);
   }
 
+  @ApiResponse({ status: 200, description: 'A doctor has been updated.' })
+  @ApiResponse({ status: 400, description: "Validation error" })
+  @ApiResponse({ status: 404, description: "Requested doctor does not exist" })
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateDoctorDto: UpdateDoctorDto) {
     return await this.doctorService.update(id, updateDoctorDto);
   }
 
+  @ApiResponse({ status: 200, description: 'A doctor has been deleted.' })
+  @ApiResponse({ status: 404, description: "Requested doctor does not exist" })
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.doctorService.remove(id);
   }
+
+
+  @ApiResponse({ status: 200, description: 'A doctor has been recovered.' })
+  @ApiResponse({ status: 404, description: "Requested doctor does not exist" })
   @Patch('recover/:id')
   async recover(@Param('id') id: string) {
     return await this.doctorService.recover(id);
--- src/modules/specialties/specialties.controller.ts
+++ src/modules/specialties/specialties.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { SpecialtiesService } from './specialties.service';
 import { CreateSpecialtyDto } from './dto/create-specialty.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 
 @ApiTags('specialties')
@@ -10,16 +10,21 @@ export class SpecialtiesController {
   constructor(private readonly specialtiesService: SpecialtiesService) { }
 
   @Post()
+  @ApiResponse({ status: 201, description: 'A specialty has been created.' })
+  @ApiResponse({ status: 400, description: "Requested specialty already exist." })
   create(@Body() createSpecialtyDto: CreateSpecialtyDto) {
     return this.specialtiesService.create(createSpecialtyDto);
   }
 
   @Get()
-  list(@Param('id') id: string) {
+  @ApiResponse({ status: 200, description: 'Successfully listed all specialties.' })
+  list() {
     return this.specialtiesService.findAll();
   }
 
   @Delete(':id')
+  @ApiResponse({ status: 200, description: 'Successfully deleted a specialty.' })
+  @ApiResponse({ status: 400, description: "Requested specialty does not exist." })
   remove(@Param('id') id: string) {
     return this.specialtiesService.remove(id);
   }
